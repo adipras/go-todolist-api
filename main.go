272ec198b82a476172934884e1add7d8e54c47b8
adipras <tottiprasetyo@gmail.com>
2024-12-02 06:41:19 +0700

    allow CORS

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"todolist/utils"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/joho/godotenv"
 )
 
@@ -17,6 +18,13 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file", err)
 	}
+
+	// Apply CORS middleware
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:5173",
+		AllowMethods: "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
+		AllowHeaders: "Origin, Content-Type, Accept, Authorization",
+	}))
 
 	config.ConnectDB()
 	migrations.Migrate()
